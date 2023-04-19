240372ae767e3930ee190cdf63a08ea08fab4d66
MuviXxx <118690549+MuviXxx@users.noreply.github.com>
2023-04-19 23:43:15 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,9 +2,71 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
+#include <cassert>
+
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+ private:
+  T* container;
+  int capacity;
+  int first, last;
+  int currentSize;
+
+ public:
+  TPQueue() :capacity(size), first(0), last(0), currentSize(0) {
+    container = new T[capacity + 1];
+  }
+
+  void push(const T& value) {
+    assert(currentSize < capacity);
+    if (currentSize == 0) {
+      container[last++] = value;
+      currentSize++;
+    } else {
+      int i = last - 1;
+      bool flag = 0;
+      while (i >= first && value.prior > container[i].prior) {
+        flag = 1;
+        container[i + 1] = container[i];
+        container[i] = value;
+        i--;
+      }
+      if (flag == 0) {
+        container[last] = value;
+      }
+      last++;
+      currentSize++;
+    }
+    if (last > capacity) {
+      last -= capacity + 1;
+    }
+  }
+
+  const T& pop() {
+    assert(currentSize > 0);
+    currentSize--;
+    if (first > capacity) {
+      first -= capacity + 1;
+    }
+    return container[first++];
+  }
+
+  char get() {
+    assert(currentSize > 0);
+    return container[first].ch;
+  }
+
+  bool isFull() const {
+    return currentSize == capacity;
+  }
+
+  bool isEmpty() const {
+    return currentSize == 0;
+  }
+
+  ~TPQueue() {
+    delete[] container;
+  }
 };
 
 struct SYM {
